4b76600625cdeac2278ccff8ee6210c1ccac569a
Kevin Martinez <msi03@laposte.net>
2017-05-09 16:56:47 +0200

    UserTable

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -9,13 +9,11 @@ class UsersController extends AppController{
     public function __construct(){
         parent::__construct();
         $this->loadModel('User');
-
     }
 
     public function index(){
-        $posts = $this->Post->last();
-        $categories = $this->Category->all();
-        $this->render('users.index', compact('posts', 'categories'));
+        $profil = $this->User->last();
+        $this->render('users.index', compact('profil'));
     }
 
     public function category(){

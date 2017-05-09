1e989b53ae21e6b619876a98f594694a30e3b23b
celebalane <celebalane@gmail.com>
2017-05-09 11:35:59 +0200

    ajout dossier img

---
+++ app/Views/templates/indexTemplate.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title><?= App::getInstance()->title; ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+<div>
+   <img src="" id="logo" /> 
+</div>
+<div class="container">
+
+    <div class="starter-template" style="padding-top: 100px;">
+        <?= $content; ?>
+    </div>
+
+</div><!-- /.container -->
+
+
+</body>
+</html>

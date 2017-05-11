126df1ec86aa4499dffdcb561afcbc90456d2b99
Sethisfir <micka_8@hotmail.fr>
2017-05-11 11:12:18 +0200

    finalisation design page accueil

--- app/Views/users/index.php
+++ app/Views/users/index.php
@@ -4,19 +4,21 @@
   <div id="profil">
     <?php echo "<img src=$src alt='profilePicture' width='150px' height='150px' id='img-profil'' />"; ?>
     <h2><?php echo "$profil->name"; ?></h2>
-    <h3>"titre"- "artiste"</h3>
-    <h3>Echangé le "dd/mm/aaaa"</h3>
-    <h3>Sur "support" avec "user".</h3>
+    <div id="artiste-head">
+      <h3>"titre"- "artiste"</h3>
+      <h3>Echangé le "dd/mm/aaaa"</h3>
+      <h3>Sur "support" avec "user".</h3>
+    </div>
     <img src="/img/Fichier 7.svg" id="img-contact">
   </div>
   <div id="content-icone">
     <div class="liste-icone">
-      <img src="img/Fichier 5.svg" alt="" width="50%">
-      <img src="img/Fichier 3.svg" alt="" width="50%">
+      <div class="col-xs-offset-1 col-xs-5 icones"><img src="img/Fichier 5.svg" alt="" width="90%"></div>
+      <div class="col-xs-offset-1 col-xs-5 icones"><img src="img/Fichier 3.svg" alt="" width="90%"></div>
     </div>
     <div class="liste-icone">
-      <img src="img/Fichier 2.svg" alt="" width="50%">
-      <img src="img/Fichier 1.svg" alt="" width="50%">
+      <div class="col-xs-offset-1 col-xs-6 icones"><img src="img/Fichier 2.svg" alt="" width="90%"></div>
+      <div class="col-xs-offset-1 col-xs-5 icones"><img src="img/Fichier 1.svg" alt="" width="90%"></div>
     </div>
   </div>
 </div>

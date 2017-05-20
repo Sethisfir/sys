a8ff5098f122dd135594f83a357cfc32607a7315
Kevin Martinez <msi03@laposte.net>
2017-05-20 21:03:25 +0200

    correction liens

--- app/Views/users/musics.php
+++ app/Views/users/musics.php
@@ -1,7 +1,7 @@
 
 <div id="echange" class="col-md-10">
 	<a class="btn btn-echange" role="button" href="index.php?p=users.request">Requète en attente</a>
-	<a class="btn btn-echange" role="button" href="index.php?p=users.searchrequest">Faire une demande</a>
+	<a class="btn btn-echange" role="button" href="index.php?p=search.index">Faire une demande</a>
 	<a class="btn btn-echange" role="button" href="index.php?p=users.share">Partages en cours</a>
 </div>
 

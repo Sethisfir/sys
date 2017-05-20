87134b940c74de5787dcbcf0dfa5b6d6716e513e
Kevin Martinez <msi03@laposte.net>
2017-05-20 18:39:03 +0200

    profilePicture

--- app/Views/users/myProfilePictures.php
+++ app/Views/users/myProfilePictures.php
@@ -1,6 +1,6 @@
 <?php
 	foreach ($pictures as $key => $value) : ?>
 		<div>
-			<img src="<?= $pictures[$key]->src ?>" alt='photo de profile' with="150" height="150" />
+			<img src="<?= $pictures[$key]->src ?>" alt='photo de profile' with="150" height="150" class="img-circle"/>
 		</div>
 <?php endforeach ?>			

4b60b43f7e23d877bef15d9019c8490f231f0d85
Kevin Martinez <msi03@laposte.net>
2017-05-21 10:39:34 +0200

    encore une modif ici aussi

--- app/Views/users/myProfilePictures.php
+++ app/Views/users/myProfilePictures.php
@@ -1,6 +1,7 @@
-<?php
-	foreach ($pictures as $key => $value) : ?>
-		<div>
+<div>
+<?php foreach ($pictures as $key => $value) : ?>
+		
 			<a href="index.php?p=users.changeProfile&picture_id= <?= $pictures[$key]->id ?>" ><img src="<?= $pictures[$key]->src ?>" alt='photo de profile' class="img-circle img-profil"/></a>
-		</div>
-<?php endforeach ?>			
+		
+<?php endforeach ?>		
+</div>	
